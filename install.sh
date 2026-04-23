590520259498e07e7d2ef83f08d5e06661278c91
RAGE <eltonrehimov6650@gmail.com>
2026-04-23 23:17:12 +0400

    Update install script for fastfetch configuration

--- install.sh
+++ install.sh
@@ -7,16 +7,16 @@ echo "⚡ Installing Rage's Fastfetch config..."
 CONFIG_DIR="$HOME/.config/fastfetch"
 REPO_URL="https://github.com/WhoamiRAGE/Rage-s-fastfetch-config.git"
 
-# klasör oluştur
+
 mkdir -p "$CONFIG_DIR"
 
-# temp klasör
+
 TMP_DIR=$(mktemp -d)
 
-# repo çek
+
 git clone "$REPO_URL" "$TMP_DIR"
 
-# config kopyala
+
 cp "$TMP_DIR/fastfetch/config.jsonc" "$CONFIG_DIR/"
 
 echo "✅ Installation complete!"
